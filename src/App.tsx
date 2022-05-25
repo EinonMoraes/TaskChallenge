2e7cf1ec077ec1f205cb5af8e98b3324217516d2
EinonMoraes <einommoares@gmail.com>
2022-05-25 13:21:44 -0300

    feat: css changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,28 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+    <section className="App">
+      <header className="header">
+        <h1>Today</h1>
+        
+
+        <div className="Tasklist-input">
+         <input
+           type="text"
+           placeholder=" O que voçê quer fazer hoje?"
+           />
+        </div>
+
+        <button type="submit" data-testid="add-task-button"  >
+
+        </button>
+        
       </header>
-    </div>
+    </section>
   )
 }
 
