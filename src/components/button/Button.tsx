71a3dbfe87eaa444f4b5e567af784909b8e5df12
EinonMoraes <einommoares@gmail.com>
2022-06-06 13:21:46 -0300

    feat: adjustment

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './styles.css'
+import check from '../../assets/check.svg'
 
 const Button: React.FC = () => {
   return ( <div>
-    <button type="submit" data-testid="add-task-button"/>
+    <button type="submit" data-testid="add-task-button">
+      <img src={check}  />
+    </button>
 </div>
   )
 }
