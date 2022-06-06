ff875f290fc4322e56e08e55b5f6d550a22cce93
EinonMoraes <einommoares@gmail.com>
2022-06-06 13:20:46 -0300

    feat: adjustment

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 import './styles.css'
 import svg from '../../assets/icon-square.svg'
+import check from '../../assets/icon-check-square.svg'
 
 
 const Item: React.FC = () => {
   return (<div className='task-line'>
-      <img src={svg}></img>
+      <img src={svg} />
       <p>Teste</p> 
   </div> 
   );
